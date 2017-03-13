343a537a6942c9e130714936c25a2a200da322de
Hector Rojas <hrpxa270@hotmail.com>
2017-03-12 21:11:07 -0600

    Adding State to the Note Component

--- app/app.jsx
+++ app/app.jsx
@@ -12,24 +12,52 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 
 var Note = React.createClass({
-    edit: function(){
-        alert('editing note');
+    getInitialState: function() {
+        return {editing: false}
+    },
+
+    edit: function() {
+        this.setState({editing: true});
+    },
+
+    save: function() {
+        this.setState({editing: false});
     },
 
     remove: function() {
         alert('removing note');
     },
 
-    render: function() {
+    renderDisplay: function() {
         return (
             <div className="note">
                 <p>{this.props.children}</p>
                 <span>
-                    <button onClick={this.edit}   className="btn btn-primary glyphicon glyphicon-pencil"/>
-                    <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
+                    <button onClick={this.edit}
+                            className="btn btn-primary glyphicon glyphicon-pencil"/>
+                    <button onClick={this.remove}
+                            className="btn btn-danger glyphicon glyphicon-trash"/>
                 </span>
             </div>
         );
+    },
+
+    renderForm: function() {
+        return (
+            <div className="note">
+                <textarea defaultValue={this.props.children} className="form-control"/>
+                <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+            </div>
+        );
+    },
+
+    render: function() {
+        if (this.state.editing) {
+            return this.renderForm();
+        }
+        else {
+            return this.renderDisplay();
+        }
     }
 });
 
