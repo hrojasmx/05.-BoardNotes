f96b87dc4998e5a7d126d869a559a5a8b2485caf
Hector Rojas <hrpxa270@hotmail.com>
2017-03-12 22:27:42 -0600

    ComponentwillMount this style

--- app/app.jsx
+++ app/app.jsx
@@ -16,6 +16,18 @@ var Note = React.createClass({
         return {editing: false}
     },
 
+    componentWillMount: function() {
+        this.style = {
+            right: this.randomBetween(0, window.innerWidth - 150) + 'px',
+            top: this.randomBetween(0, window.innerHeight - 150) + 'px',
+            transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
+        };
+    },
+
+    randomBetween: function(min, max) {
+        return (min + Math.ceil(Math.random() * max));
+    },
+
     edit: function() {
         this.setState({editing: true});
     },
@@ -36,7 +48,7 @@ var Note = React.createClass({
 
     renderDisplay: function() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit}
@@ -50,7 +62,7 @@ var Note = React.createClass({
 
     renderForm: function() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <textarea ref="newText" defaultValue={this.props.children} className="form-control"/>
                 <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
             </div>
@@ -101,15 +113,23 @@ var Board = React.createClass({
         };
     },
 
+    nextId: function() {
+        this.uniqueId = this.uniqueId || 0;
+        return this.uniqueId++;
+    },
+
     add: function(text) {
         var arr = this.state.notes;
-        arr.push(text);
+        arr.push({
+            id: this.nextId(),
+            note: text
+        });
         this.setState({notes: arr});
     },
 
     update: function(newText, i) {
         var arr = this.state.notes;
-        arr[i] = newText;
+        arr[i].note = newText;
         this.setState({notes:arr});
     },
 
@@ -121,11 +141,11 @@ var Board = React.createClass({
 
     eachNote: function(note, i) {
         return (
-            <Note key={i}
+            <Note key={note.id}
                   index={i}
                   onChange={this.update}
                   onRemove={this.remove}
-            >{note}</Note>
+            >{note.note}</Note>
         );
     },
 
