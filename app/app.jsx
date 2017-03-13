8af3d099fc2d3d0c2deb5162665c9a1f2169dd7e
Hector Rojas <hrpxa270@hotmail.com>
2017-03-12 22:06:45 -0600

    Updating and Removing Notes

--- app/app.jsx
+++ app/app.jsx
@@ -22,14 +22,16 @@ var Note = React.createClass({
 
     save: function() {
         // var val = this.refs.newText.getDOMNode().value;
-        var val = this.refs.newText.value;
+        // var val = this.refs.newText.value;
 
-        alert("TODO: Save note value:->" + val);
+        //this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
+        this.props.onChange(this.refs.newText.value, this.props.index);
         this.setState({editing: false});
     },
 
     remove: function() {
-        alert('removing note');
+        // alert('removing note');
+        this.props.onRemove(this.props.index);
     },
 
     renderDisplay: function() {
@@ -104,14 +106,33 @@ var Board = React.createClass({
         };
     },
 
+    update: function(newText, i) {
+        var arr = this.state.notes;
+        arr[i] = newText;
+        this.setState({notes:arr});
+    },
+
+    remove: function(i) {
+        var arr = this.state.notes;
+        arr.splice(i, 1);
+        this.setState({notes: arr});
+    },
+
+    eachNote: function(note, i) {
+        return (
+            <Note key={i}
+                  index={i}
+                  onChange={this.update}
+                  onRemove={this.remove}
+            >{note}</Note>
+        );
+    },
+
     render: function() {
-        return (<div className="board">
-                {this.state.notes.map(function(note, i) {
-                    return (
-                        <Note key={i}>{note}</Note>
-                    );
-                })}
-            </div>
+        return (
+                <div className="board">
+                    {this.state.notes.map(this.eachNote)}
+                </div>
         );
     }
 });
