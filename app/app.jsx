e59d6c2f8f2f97abd2c53a791b41801c1b964869
Hector Rojas <hrpxa270@hotmail.com>
2017-03-12 21:03:13 -0600

    Adding Hello World to the Note

--- app/app.jsx
+++ app/app.jsx
@@ -23,6 +23,7 @@ var Note = React.createClass({
     render: function() {
         return (
             <div className="note">
+                <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit}   className="btn btn-primary glyphicon glyphicon-pencil"/>
                     <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
@@ -32,6 +33,6 @@ var Note = React.createClass({
     }
 });
 
-ReactDOM.render(<Note />,
+ReactDOM.render(<Note>Hello World</Note>,
     document.getElementById('app')
 );
